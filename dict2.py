7b89e0b2cd37902efdbf67244e65947715ec92ee
Stephane Tran <71317608+StephaneTran@users.noreply.github.com>
2021-07-23 20:17:15 +1000

    Update to dict2.py
    Creating dictionary using mysql as database. Adding conditionals to make it more user friendly.

--- dict2.py
+++ dict2.py
@@ -1,16 +1,46 @@
 # Dictionary using mysql database
 import mysql.connector
+from difflib import get_close_matches
 
 con = mysql.connector.connect(
     user = "ardit700_student",
-    password = "adit700_student",
+    password = "ardit700_student",
     host = "108.167.140.122",
     database = "ardit700_pm1database"
 )
 
 cursor = con.cursor()
 
-query = cursor.execute("Select * FROM Dictionary WHERE Expression = 'ghost' ")
-results = cursor.fetchall()
+def get_def(word):
+    query = cursor.execute(f"Select * FROM Dictionary WHERE Expression = '{word}'")
+    results = cursor.fetchall()
+    query2 = cursor.execute(f"Select * FROM Dictionary WHERE Expression LIKE '{word[0]}%{word[-1]}'")
+    results2 = cursor.fetchall()
+    alike_words = [tuple1[0] for tuple1 in results2]
+    alike_def = [tuple1[1] for tuple1 in results2]
+    x = get_close_matches(word, alike_words)
+    if results: 
+        for result in results:
+            print(result[1])
+    elif len(x) > 0:
+        yn = input(f"Did you mean {x[0]} instead? Enter Y if yes, or N if no: ")
+        if yn == "Y" or yn == "y":
+            query3 = cursor.execute(f"Select * FROM Dictionary WHERE Expression = '{x[0]}'")
+            results3 = cursor.fetchall()
+            print(results3[0][1])
+        elif yn == "N" or yn == "n":
+            print("Word does not exist. Please re-enter a word.")
+        else: 
+            print("We do not understand your entry.")    
+    else:
+        print("Word does not exist.")
 
-print(results)
+while True:
+    word = input("Enter a word: ")
+    if word == "\end":
+        break
+    else:
+        get_def(word)
+    continue
+
+
